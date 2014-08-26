65e607f2855a6801b8f2b0e377c1380460504846
Richard Membarth <richard.membarth@uni-saarland.de>
2014-08-26 16:51:48 +0200

    Add main file for aobench.

---
+++ aobench/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <thorin_runtime.h>
+
+#include "pnm_image.h"
+
+extern "C" void thorin_ao();
+
+extern "C"
+void saveppm(uint8_t *img, int width, int height) {
+    write_pnm_image(img, width, height, "ao.ppm", pnm_t::P6);
+}
+
+/*************************************************************************
+ * Main function                                                         *
+ *************************************************************************/
+int main(int argc, const char **argv) {
+    // initialize AnyDSL runtime
+    thorin_init();
+
+    std::cout << "Calculating aobench in AnyDSL ..." << std::endl;
+    thorin_ao();
+
+    return EXIT_SUCCESS;
+}
+
