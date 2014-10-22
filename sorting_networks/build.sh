63429f989fd4c77c50976dcb0e65e8e7efbfc211
Richard Membarth <richard.membarth@uni-saarland.de>
2014-10-22 15:13:30 +0200

    Median filter with CPU mapping.

--- sorting_networks/build.sh
+++ sorting_networks/build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-impala ../intrinsics_utils.impala ../extern.impala oddeven.impala -emit-llvm
+impala ../intrinsics_thorin.impala ../intrinsics_utils.impala ../extern.impala oddeven.impala -emit-llvm
+impala ../intrinsics_thorin.impala ../intrinsics_utils.impala ../extern.impala ../intrinsics_cpu.impala mapping_cpu.impala oddeven.impala median.impala -emit-llvm
