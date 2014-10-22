30773ae646735584a28519bf138996a3991d55e8
Richard Membarth <richard.membarth@uni-saarland.de>
2014-10-22 16:24:18 +0200

    GPU mapping for median filter.

--- sorting_networks/build.sh
+++ sorting_networks/build.sh
@@ -2,3 +2,4 @@
 
 impala ../intrinsics_thorin.impala ../intrinsics_utils.impala ../extern.impala oddeven.impala -emit-llvm
 impala ../intrinsics_thorin.impala ../intrinsics_utils.impala ../extern.impala ../intrinsics_cpu.impala mapping_cpu.impala oddeven.impala median.impala -emit-llvm
+impala ../intrinsics_thorin.impala ../intrinsics_utils.impala ../extern.impala ../intrinsics_nvvm.impala mapping_acc.impala oddeven.impala median.impala -emit-llvm
