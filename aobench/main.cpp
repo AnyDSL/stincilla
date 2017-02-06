3b980365cc83ced6fa46da24d9835aacc68f8602
Stefan Lemme <stefan.lemme@dfki.de>
2017-02-06 10:29:19 +0100

    Add polyfill of drand48() to aobench for msvc

--- aobench/main.cpp
+++ aobench/main.cpp
@@ -11,6 +11,17 @@ void saveppm(uint8_t* img, int width, int height) {
     write_pnm_image(img, width, height, "aobench.ppm", pnm_t::P6);
 }
 
+// polyfill of non-standard drand48()
+#ifdef _MSC_VER
+
+#include <random>
+static std::mt19937_64 std_gen64;
+static std::uniform_real_distribution<double> std_dist64(0., 1.);
+extern "C" void srand48(int64_t seed) { std_gen64.seed(seed); }
+extern "C" double drand48() { return std_dist64(std_gen64); }
+
+#endif // _MSC_VER
+
 /*************************************************************************
  * Main function                                                         *
  *************************************************************************/
