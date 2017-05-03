0b3dcc77796bfb86184e5894759fd1a95232ab9b
Richard Membarth <richard.membarth@uni-saarland.de>
2017-05-03 13:18:48 +0200

    Remove obsolete file.

--- sorting_networks/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-impala ../intrinsics_thorin.impala ../intrinsics_utils.impala ../extern.impala oddeven.impala -emit-llvm
-impala ../intrinsics_thorin.impala ../intrinsics_utils.impala ../extern.impala ../intrinsics_cpu.impala mapping_cpu.impala oddeven.impala median.impala -emit-llvm
-impala ../intrinsics_thorin.impala ../intrinsics_utils.impala ../extern.impala ../intrinsics_nvvm.impala mapping_acc.impala oddeven.impala median.impala -emit-llvm
