f9021572919d0c472a629b286dc9b6c555f0d59b
akifoezkan <akif.oezkan@fau.de>
2017-12-12 18:15:22 +0100

    harris corner is modified for pixel_t

--- harris_corner/main.cpp
+++ harris_corner/main.cpp
@@ -4,7 +4,7 @@
 
 #include "pnm_image.h"
 
-extern "C" void harris_corner(float*, float*, int, int);
+extern "C" void harris_corner(float*, float*);
 
 /*************************************************************************
  * Main function                                                         *
@@ -22,7 +22,7 @@ int main(int argc, const char **argv) {
     // initialize data
     for (size_t i=0; i<width*height; ++i) input[i] = image[i];
 
-    harris_corner(input.data(), output.data(), width, height);
+    harris_corner(input.data(), output.data());
 
     // write image
     for (size_t i=0; i<width*height; ++i) image[i] = (uint8_t)output[i];
