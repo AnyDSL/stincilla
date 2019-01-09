a2372249a6df549881c6e0c128f9654203c30352
Puya Amiri <puya.amiri@dfki.de>
2019-01-09 16:31:20 +0100

    Clean up

--- scripts/utils/ctags_update.sh
+++ scripts/utils/ctags_update.sh
@@ -3,8 +3,8 @@
 FILE=.ctags
 if [ $(dpkg-query -W -f='${Status}' exuberant-ctags 2>/dev/null | grep -c "ok installed") -eq 0 ];
 then
-  apt-get install exuberant-ctags;
-  grep -q -x -F 'set tags=./tags;' ~/.vimrc || echo 'set tags=./tags;' >> ~/.vimrc
+    apt-get install exuberant-ctags;
+    grep -q -x -F 'set tags=./tags;' ~/.vimrc || echo 'set tags=./tags;' >> ~/.vimrc
     if [ -f ~/$FILE ]; then
         echo '--langdef=impala' >> ~/$FILE
         echo '--langmap=impala:.impala' >> ~/$FILE
@@ -22,10 +22,7 @@ then
         echo '--regex-impala=/^[ \t]*fn[ \t]([a-zA-Z0-9_]+)/\1/f,function/' >> ~/$FILE
         echo '--regex-impala=/^[ \t]*let[ \t]+([a-zA-Z0-9_]+)[ \t]*=/\1/c,constant/' >> ~/$FILE
         echo '--regex-impala=/^[ \t]*let[ \t]+mut[ \t]+([a-zA-Z0-9_]+)[ \t]*=/\1/v,variable/' >> ~/$FILE
-
     fi
-
-
 fi
 
 ctags -R --exclude=impala --exclude=llvm --exclude=llvm_install --exclude=llvm_build --exclude=traversal --exclude=half --exclude=anyseq --exclude=thorin .
