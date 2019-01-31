86979014971f2e2ed3fde636f65863992e0a9e14
Puya Amiri <puya.amiri@dfki.de>
2019-01-31 18:37:05 +0100

    Simplified code for pointwise operations (Square)

--- scripts/board/Xilinx/init_xilinx.sh
+++ scripts/board/Xilinx/init_xilinx.sh
@@ -1,4 +1,10 @@
-
+mode=$1
 #export SELECTED_BOARD=zcu
 export SELECTED_VERSION=2018.2
-source /opt/Xilinx/Vivado/${SELECTED_VERSION}/settings64.sh
+path="/opt/Xilinx"
+source $path/Vivado/${SELECTED_VERSION}/settings64.sh
+if [ "$1" = "soc" ];
+then
+    echo "SDSoC got initialized"
+    source $path/SDx/${SELECTED_VERSION}/settings64.sh
+fi
