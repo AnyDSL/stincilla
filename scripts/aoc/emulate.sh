243240741109ab19d517d5ed1ef31777ebb8984d
Puya Amiri <pooya.w@gmail.com>
2019-10-06 01:23:52 +0200

    updating emulation script for quartus 19

--- scripts/aoc/emulate.sh
+++ scripts/aoc/emulate.sh
@@ -15,4 +15,10 @@ else
 fi
 aoc -report -march=emulator $APP.cl -o $dir/$APP.aocx
 ln -sf $dir/$APP.aocx ./$APP.aocx
-CL_CONTEXT_EMULATOR_DEVICE_INTELFPGA=1 ./$APP
+
+if [ "$SELECTED_VERSION" == "19.*" ]
+then
+    CL_CONFIG_CPU_EMULATE_DEVICES=1 ./$APP
+else
+    CL_CONTEXT_EMULATOR_DEVICE_INTELFPGA=1 ./$APP
+fi
