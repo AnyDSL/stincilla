193397cf054e3718f4027a03e993fa9061b150b1
Richard Membarth <richard.membarth@uni-saarland.de>
2020-08-31 18:11:28 +0200

    Port to artic.

--- test/jit.cpp
+++ test/jit.cpp
@@ -7,25 +7,24 @@ typedef int(*exec_cpu_fn)();
 typedef int(*exec_gpu_fn)();
 
 int main(int argc, const char **argv) {
-    std::string program_cpu = "extern fn get_42() -> i32 { 42 }";
+    std::string program_cpu = "#[export] fn get_42() -> i32 { 42 }";
     std::string program_gpu =
-        "extern fn get_42() -> i32 {"
-        "   let (platform, device, codegen) = (2, 1, opencl);"
-        "   /*let (platform, device, codegen) = (1, 1, cuda);*/"
-        "   /*let (platform, device, codegen) = (1, 1, nvvm);*/"
-        "   /*let (platform, device, codegen) = (3, 1, amdgpu);*/"
+        "#[export] fn get_42() -> i32 {"
+        "   let (platform, device, codegen) = (2, 0, opencl);"
+        "   /*let (platform, device, codegen) = (1, 0, cuda);*/"
+        "   /*let (platform, device, codegen) = (1, 0, nvvm);*/"
+        "   /*let (platform, device, codegen) = (3, 0, amdgpu);*/"
         "   let size = sizeof[i32]();"
-        "   let dev = 0;"
         "   let dev_cpu  = runtime_device(0, 0);"
         "   let dev_gpu  = runtime_device(platform, device);"
         "   let data_cpu = runtime_alloc(dev_cpu, size);"
         "   let data_gpu = runtime_alloc(dev_gpu, size);"
         "   let ptr_cpu = bitcast[&mut[i32]](data_cpu);"
-        "   let ptr_gpu = bitcast[&mut[1][i32]](data_gpu);"
+        "   let ptr_gpu = bitcast[&mut addrspace(1)[i32]](data_gpu);"
         "   ptr_cpu(0) = 0;"
-        "   runtime_copy(dev_cpu, data_cpu, 0i64, dev_gpu, data_gpu, 0i64, size);"
+        "   runtime_copy(dev_cpu, data_cpu, 0, dev_gpu, data_gpu, 0, size);"
         "   codegen(device, (1, 1, 1), (1, 1, 1), || { ptr_gpu(0) = 42; });"
-        "   runtime_copy(dev_gpu, data_gpu, 0i64, dev_cpu, data_cpu, 0i64, size);"
+        "   runtime_copy(dev_gpu, data_gpu, 0, dev_cpu, data_cpu, 0, size);"
         "   let res = ptr_cpu(0);"
         "   runtime_release(dev_cpu, data_cpu);"
         "   runtime_release(dev_gpu, data_gpu);"
